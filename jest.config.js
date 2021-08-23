af0adfc1b70863970a8f11c934171ccce8b1a282
Joe-noh <goflb.jh@gmail.com>
2021-08-24 08:59:25 +0900

    remove svelte related packages

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
-const { preprocess } = require('./svelte.config.js')
-
 module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '^.+\\.svelte$': ['jest-transform-svelte', { preprocess }],
   },
-  moduleFileExtensions: ['js', 'svelte'],
+  moduleFileExtensions: ['js'],
 }
