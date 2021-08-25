880ddc9458d307dce013089e962b9577e0cb43b8
Joe-noh <goflb.jh@gmail.com>
2021-08-25 17:19:38 +0900

    preview components

--- src/elements/bl-button.ts
+++ src/elements/bl-button.ts
@@ -1,9 +1,13 @@
-import { html, css, LitElement } from 'lit'
+import { LitElement, html, css } from 'lit'
 import { customElement } from 'lit/decorators.js'
 
 @customElement('bl-button')
 export class BlButton extends LitElement {
-  static styles = css`p { color: blue }`
+  static styles = css`
+    button {
+      color: resd;
+    }
+  `
 
   render() {
     return html`<button><slot></slot></button>`
