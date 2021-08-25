34ab0900c79fdab9881f2180e12ae627ef46964d
Joe-noh <goflb.jh@gmail.com>
2021-08-26 02:15:21 +0900

    build umd and esm

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,17 @@ import typescript from '@rollup/plugin-typescript'
 
 export default {
   input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'esm',
-  },
+  output: [
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+    },
+    {
+      file: 'dist/index.umd.js',
+      name: 'bolg-components',
+      format: 'umd',
+    },
+  ],
   plugins: [
     resolve(),
     typescript({ sourceMap: false }),
