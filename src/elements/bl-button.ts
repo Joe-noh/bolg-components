200141e84022b6e04932a534cd5ace8ab189dc57
Joe-noh <goflb.jh@gmail.com>
2021-08-25 22:02:54 +0900

    support some button type and size

--- src/elements/bl-button.ts
+++ src/elements/bl-button.ts
@@ -5,6 +5,13 @@ import { classMap } from 'lit/directives/class-map.js';
 @customElement('bl-button')
 export class BlButton extends LitElement {
   static styles = css`
+    :host {
+      --primary-color: #e3564f;
+      --button-text-color: #fafafa;
+      --font-size-small: 14px;
+      --font-size-normal: 16px;
+    }
+
     button {
       display: flex;
       flex-flow: row nowrap;
@@ -12,6 +19,11 @@ export class BlButton extends LitElement {
       justify-content: center;
       border: solid 1px var(--primary-color);
       border-radius: 5px;
+      cursor: pointer;
+    }
+    button:disabled {
+      opacity: 0.5;
+      cursor: not-allowed;
     }
 
     .sizeNormal {
@@ -22,17 +34,41 @@ export class BlButton extends LitElement {
       padding: 0.3rem 1rem;
       font-size: var(--font-size-small);
     }
+
+    .typePrimary {
+      background-color: var(--primary-color);
+      color: var(--button-text-color);
+    }
+    .typeOutline {
+      background-color: transparent;
+      color: var(--primary-color);
+    }
   `
 
   @property({ type: String })
-  size: string = 'normal';
+  size: string = 'normal'
+
+  @property({ type: String })
+  type: string = 'primary'
+
+  @property({ type: String })
+  nativeType: string = 'button'
+
+  @property({ type: Boolean })
+  disabled: boolean = false
 
   render() {
     const classes = {
       sizeSmall: this.size === 'small',
       sizeNormal: this.size === 'normal',
+      typePrimary: this.type === 'primary',
+      typeOutline: this.type === 'outline',
     }
 
-    return html`<button class=${classMap(classes)}><slot></slot></button>`
+    return html`
+      <button class=${classMap(classes)} type=${this.nativeType} ?disabled=${!!this.disabled}>
+        <slot></slot>
+      </button>
+    `
   }
 }
