cce1fc7faccd0c287bd9df2f0b70e9f259d61e6a
Joe-noh <goflb.jh@gmail.com>
2021-08-25 20:43:14 +0900

    button size prop

--- src/elements/bl-button.ts
+++ src/elements/bl-button.ts
@@ -1,15 +1,38 @@
 import { LitElement, html, css } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { customElement, property } from 'lit/decorators.js'
+import { classMap } from 'lit/directives/class-map.js';
 
 @customElement('bl-button')
 export class BlButton extends LitElement {
   static styles = css`
     button {
-      color: resd;
+      display: flex;
+      flex-flow: row nowrap;
+      align-items: center;
+      justify-content: center;
+      border: solid 1px var(--primary-color);
+      border-radius: 5px;
+    }
+
+    .sizeNormal {
+      padding: 0.5rem 1.5rem;
+      font-size: var(--font-size-normal);
+    }
+    .sizeSmall {
+      padding: 0.3rem 1rem;
+      font-size: var(--font-size-small);
     }
   `
 
+  @property({ type: String })
+  size: string = 'normal';
+
   render() {
-    return html`<button><slot></slot></button>`
+    const classes = {
+      sizeSmall: this.size === 'small',
+      sizeNormal: this.size === 'normal',
+    }
+
+    return html`<button class=${classMap(classes)}><slot></slot></button>`
   }
 }
