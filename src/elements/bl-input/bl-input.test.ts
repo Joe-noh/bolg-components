7fe4fa39105c66ad864136f2575d45696e95d69c
Joe-noh <goflb.jh@gmail.com>
2021-09-11 17:39:58 +0900

    Fix could not import test module

--- src/elements/bl-input/bl-input.test.ts
+++ src/elements/bl-input/bl-input.test.ts
@@ -1,6 +1,6 @@
 import { expect, fixture, html } from '@open-wc/testing'
 import { BlInput } from './bl-input'
-import '../../../dist'
+import '../../../'
 
 describe('BlInput', () => {
   it('set given value', async () => {
