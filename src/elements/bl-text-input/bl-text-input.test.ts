117af79faf312792572c3b8c7fa34f79a4cb3950
Joe-noh <goflb.jh@gmail.com>
2021-09-11 16:47:29 +0900

    implement bl-text-input

---
+++ src/elements/bl-text-input/bl-text-input.test.ts
@@ -0,0 +1,24 @@
+import { expect, fixture, html } from '@open-wc/testing'
+import { BlTextInput } from './bl-text-input'
+import '../../../'
+
+describe('BlTextInput', () => {
+  it('set given value', async () => {
+    const el: BlTextInput = await fixture(html`<bl-text-input value="Hello" />`)
+
+    expect(el.input.value).to.equal('Hello')
+    expect(el.input.disabled).to.equal(false)
+  })
+
+  it('set given placeholder', async () => {
+    const el: BlTextInput = await fixture(html`<bl-text-input placeholder="Hey" />`)
+
+    expect(el.input.placeholder).to.equal('Hey')
+  })
+
+  it('disable input element', async () => {
+    const el: BlTextInput = await fixture(html`<bl-text-input disabled />`)
+
+    expect(el.input.disabled).to.equal(true)
+  })
+})
